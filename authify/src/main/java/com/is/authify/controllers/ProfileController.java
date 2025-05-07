5ccab9e7100b7984e7f13290a98bb07a7f29a134
azelio5 <a.alakbarov@gmail.com>
2025-05-07 17:25:42 +0300

    Profile Controller

---
+++ authify/src/main/java/com/is/authify/controllers/ProfileController.java
@@ -0,0 +1,24 @@
+package com.is.authify.controllers;
+
+import com.is.authify.io.ProfileRequest;
+import com.is.authify.io.ProfileResponse;
+import com.is.authify.services.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1.0")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProfileResponse register(@RequestBody final ProfileRequest request) {
+        ProfileResponse response = profileService.createProfile(request);
+        //TODO: welcome email
+        return response;
+    }
+}
